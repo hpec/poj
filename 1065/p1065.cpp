a99c62c3e9222bcf334a1cecf9ab2f9b79c5cf2d
Yilin <hpec.liu@gmail.com>
2013-01-20 06:16:46 -0800

    p1065 updated

--- 1065/p1065.cpp
+++ 1065/p1065.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int l[5000], w[5000];
-int i, n, t, cw, cl, ans;
+int l[5000], w[5000], visited[5000];
+int i, j, n, t, cw, cl, ans, tmp;
 
 void qsort(int p, int q){
     int i, j, tmp, midl, midw;
@@ -10,7 +10,7 @@ void qsort(int p, int q){
     j = q;
     midl = l[(i + j) / 2];
     midw = w[(i + j) / 2];
-    while (i < j){
+    while (i <= j){
         while (l[i] < midl || (l[i] == midl && w[i] < midw)) i++;
         while (l[j] > midl || (l[j] == midl && w[j] > midw)) j--;
         if (i <= j){
@@ -32,19 +32,27 @@ int main(){
     while (t--){
         cin >> n;
         ans = 0;
-        cl = 1000000;
-        cw = 1000000;
+        cl = 20000;
+        cw = 20000;
+        memset(visited, 0, sizeof(visited));
         for (i = 0; i < n; i++){
             cin >> l[i] >> w[i];
         }
 
         qsort(0, n-1);
         for (i = 0; i < n; i++){
-            if (!(l[i] >= cl && w[i] >= cw)){
+            if (!visited[i]){
                 ans++;
-                cl = l[i];
-                cw = w[i];
+                tmp = w[i];
+                visited[i] = true;
+                for (j = i + 1; j < n; j++){
+                    if (!visited[j] && w[j] >= tmp){
+                        visited[j] = true;
+                        tmp = w[j];
+                    }
+                }
             }
+
         }
         cout << ans << endl; 
     }
